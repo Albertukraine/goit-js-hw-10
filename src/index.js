9460d426371b2b474b9bf304de30872e8c8b38d7
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-20 10:45:21 +0300

    fix syntax at li params

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import "./css/styles.css";
-import debounce from "lodash.debounce";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-import { fetchCountries } from "./fetchCountries.js"; 
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-  formEl: document.querySelector("#search-box"),
-  listEl: document.querySelector(".country-list"),
-  listItemEl: document.querySelector(".country-info"),
+  formEl: document.querySelector('#search-box'),
+  listEl: document.querySelector('.country-list'),
+  listItemEl: document.querySelector('.country-info'),
 };
 
 // console.log(refs);
@@ -18,27 +18,25 @@ function onInput(event) {
   let normalizedCountry = country.trim();
   if (normalizedCountry.length === 0) {
     // console.log("clear window");
-    refs.listItemEl.innerHTML = "";
-    refs.listEl.innerHTML = "";
+    refs.listItemEl.innerHTML = '';
+    refs.listEl.innerHTML = '';
     return;
   } else {
     // console.log(normalizedCountry);
     fetchCountries(normalizedCountry)
-      .then((response) => {
-        return response.json()
+      .then(response => {
+        return response.json();
       })
-      .then((countries) => renderHTML(countries))
-      .catch((error) =>
-        Notify.failure("Oops, there is no country with that name")
+      .then(countries => renderHTML(countries))
+      .catch(error =>
+        Notify.failure('Oops, there is no country with that name')
       );
   }
 }
 
 function renderHTML(countries) {
   if (countries.length > 10) {
-    Notify.info(
-      "Too many matches found. Please enter a more specific name."
-    );
+    Notify.info('Too many matches found. Please enter a more specific name.');
     return;
   }
   for (const country of countries) {
@@ -48,31 +46,25 @@ function renderHTML(countries) {
     }
     // console.log(langTypes);
     if ((countries.length <= 10) & (countries.length > 1)) {
-    //   console.log("RENDER LIST ITEM");
+      //   console.log("RENDER LIST ITEM");
 
-      const listMarkup = countries.map((country) => {
-        return `<li> <img src = ${country.flags.svg} height = 20px>
-            ${country.name}</li>`
+      const listMarkup = countries.map(country => {
+        return `<li> <img src = "${country.flags.svg}" height = "20px">${country.name}</li>`;
       });
-      refs.listItemEl.innerHTML = " ";
+      refs.listItemEl.innerHTML = ' ';
       refs.listEl.innerHTML = listMarkup;
     } else if (countries.length === 1) {
       const countryCard = `<div>  
-<h2><img src = ${country.flags.svg} height = 30px> ${country.name}</h2>
+<h2><img src = "${country.flags.svg}" height = "30px"> ${country.name}</h2>
 <p><b>Capital:</b> ${country.capital}</p>
 <p><b>Population:</b> ${country.population}</p>
-<p><b>Languages:</b> ${langTypes.join(", ")}</p>
+<p><b>Languages:</b> ${langTypes.join(', ')}</p>
 </div>`;
-    //   console.log("RENDER CARD");
-    refs.listEl.innerHTML = " ";
+      //   console.log("RENDER CARD");
+      refs.listEl.innerHTML = ' ';
       refs.listItemEl.innerHTML = countryCard;
     }
   }
 }
 
-refs.formEl.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
-
-
-
-
-
+refs.formEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
